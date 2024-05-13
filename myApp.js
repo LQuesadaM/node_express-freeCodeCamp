e224133e87491da85905b628ab3eb3f8910896b3
LQuesadaM <luisquesada163@gmail.com>
2024-05-13 17:52:27 -0300

    🤡  test

--- myApp.js
+++ myApp.js
@@ -1,39 +1,24 @@
-let express = require('express');
-let app = express();
+const express = require('express')
+const app = express()
+const path = require('node:path')
+module.exports = app
 
+const absolutePathIndex = path.join(__dirname, 'views', 'index.html')
+const absolutePathCss = path.join(__dirname, 'public')
+/*
+app.get('/', function (req, res) {
+  res.send('Hello Express')
+})
+*/
 
+// show index
+app.get('/', function (req, res) {
+  res.sendFile(absolutePathIndex)
+})
 
+// access style.css
+app.use('/public', express.static(absolutePathCss))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+app.get('/json', function (req, res) {
+  res.json()
+})
